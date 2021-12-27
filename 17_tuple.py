255b1224f477732f2a6d491d071314cdfcfb082e
ITketan <gohel.ketan192001@gmail.com>
2021-12-27 10:35:38 +0530

    changes commit

---
+++ 17_tuple.py
@@ -0,0 +1,8 @@
+#python program to demonstrate creation of tuple
+#creating an empty tuple
+tuple1 = ()
+print("initial empty tuple: ")
+print(tuple1)
+
+#creating tuple with the use of strings
+tuple2 = ('')
