9815d662368d076e5af49e29eefcd2b3bb3d94fc
ITketan <gohel.ketan192001@gmail.com>
2021-12-31 14:08:04 +0530

    tuple

--- 17_tuple.py
+++ 17_tuple.py
@@ -6,3 +6,25 @@ print(tuple1)
 
 #creating tuple with the use of strings
 tuple2 = ('python','for','programmers')
+print("\nTuple with the use of string:")
+print(tuple2)
+
+#creting a tuple with the use of list
+list1 = [1,2,3,4,5,6]
+print("\nTuple using list")
+print(tuple(list1))
+
+#creating a tuple with the use of built-in function
+tuple1 = tuple('king')
+print("\nTuple with the use of function:")
+print(tuple1)
+
+#creating a tuple with neseted tuples
+tuple1 = (0,1,2,3)
+tuple2 = ('python','king')
+tuple3 = (tuple1,tuple2)
+print("\nTuple with nested tuples:")
+print(tuple3)
+
+
+
---
+++ 18_accessing_tuple.py
@@ -0,0 +1,12 @@
+#python program to demostrate accessing tuple
+tuple1 = tuple([1,2,3,4,5])
+#accessing element using indexing
+print("First element of tuple")
+print(tuple1[0])
+
+#accessing element from last negative indexing
+print("\nLast element of tuple")
+print(tuple1[-1])
+
+print("\nThird last element of tuple")
+print(tuple1[-3])
