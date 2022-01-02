6794dd6f49af3c8cc9c4b3156f0bab6938d86794
ITketan <gohel.ketan192001@gmail.com>
2022-01-02 13:54:18 +0530

    branching statements

---
+++ 22_if_statement.py
@@ -0,0 +1,6 @@
+#if statement
+password = "qwerty"
+attempt = input("Enter password: ")
+if attempt == password:
+    print("Welcome")
+print("if statement")
---
+++ 24_if_else_example.py
@@ -0,0 +1,7 @@
+#example
+password = "ketan"
+attempt = input("Enter password:")
+if attempt == password:
+    print("Welcome")
+else:
+    print("Incorrect password!")
