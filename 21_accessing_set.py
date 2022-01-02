3aeb779dbc73f9290e77cf887fc412a0854fe874
ITketan <gohel.ketan192001@gmail.com>
2022-01-02 13:31:37 +0530

    add new

--- 21_accessing_set.py
+++ 21_accessing_set.py
@@ -1,3 +1,23 @@
 #python program to demonstrate Accessing of elements in a set
 #creation a set
-set1 = 
+set1 = set(['Goal','for','life'])
+print("\nInitial set")
+print(set1)
+
+#accessing element using for loop
+print("\nElement of set:")
+for i in set1:
+    print(i, end=" ")
+print("\n")
+
+#checking the element using in keyword
+print("checking the element using in keyword ")
+print("Goal" in set1)
+
+a = {5,2,3,1,4}
+
+#printing set variable
+print("\na=",a)
+
+#data type of variable a
+print(type(a))
