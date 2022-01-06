738716d7870ba58302c747700b34870c9a05cb6b
ITketan <gohel.ketan192001@gmail.com>
2022-01-06 16:42:43 +0530

    new

---
+++ 53_example.py
@@ -0,0 +1,23 @@
+
+
+def apply(L,f):
+    """
+    Applies function given by f to each element in L
+    parameters
+    --------------
+    L: list containing the operands
+    f: the function
+    Returns
+    --------------
+    result:resulting list
+    """
+    result = []
+    for i in range(len(L)):
+        result.append(f(L[i]))
+
+    return result
+L = [1,-2,-5,6.2]
+print(apply(L,abs))  #[1,2,5,6.2]
+#abs is applied on elements passed in L
+print(apply(L,int)) #[1,-2,-5,6]
+    
---
+++ 54_dictionary.py
@@ -0,0 +1,11 @@
+#empty dictionary
+my_dict = {}
+print(my_dict)
+
+#dictionary with integer keys
+my_dict = {1:'apple',2:'ball'}
+print(my_dict)
+
+#dictionary with mixed keys
+my_dict = {'name':'john',1:[2,4,3]}
+print(my_dict)
